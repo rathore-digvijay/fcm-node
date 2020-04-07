fd5b85a932e27781b14044fba6e18f261ada5e8e
nj <namanj975@gmail.com>
2020-04-07 18:22:09 +0530

    updated cofigs

--- fcm-notify.js
+++ fcm-notify.js
@@ -8,8 +8,8 @@
 // Add the SDK
 const fcm_admin = require('firebase-admin');
 var serviceAccount = require("./fcmConfig.json");
-var token = "czh7BEAcMOD2ed9xTzHOaP:APA91bFC3QHbwCUaQtrS-XCnliom74i38ckAu3_96AjZw0CQCq1ZzUtitohkw_NOqTtYU7ll7zByjuiKO8lbvHFe2TI-H7oTmXNPZSP5KV6_tv3ZZRc8OLnJ9TGkukzG3sq-oDwF2Nbi";
-
+// var token = "czh7BEAcMOD2ed9xTzHOaP:APA91bFC3QHbwCUaQtrS-XCnliom74i38ckAu3_96AjZw0CQCq1ZzUtitohkw_NOqTtYU7ll7zByjuiKO8lbvHFe2TI-H7oTmXNPZSP5KV6_tv3ZZRc8OLnJ9TGkukzG3sq-oDwF2Nbi";
+var token = "abcdfres";
 /**
  * Initialize the SDK
  */
@@ -24,27 +24,9 @@ function initializeFcm() {
     // });
     fcm_admin.initializeApp({
         credential: fcm_admin.credential.cert(serviceAccount),
-        databaseURL: "https://slots-push-notification-test.firebaseio.com"
+        databaseURL: "https://slots-push-notification-test1.firebaseio.com"
     });
 }
-
-/**
- * This message send the notification to specified users whose registration token is placed here.
- * For ref: https://firebase.google.com/docs/reference/admin/node/admin.messaging.Messaging
- * @param {Array | String} registrationTokens A device registration token or an array of device registration tokens to which the message should be sent.
- * @param {Object} payload Message Object
- * @param {Object} options Optional, for options while sending message
- */
-// function sendToDevice(registrationTokens, payload, options = {}) {
-//     console.log("push notification in fcm-notify");
-//     fcm_admin.messaging().sendToDevice(registrationTokens, payload, options)
-//         .then(function (response) {
-//             console.log("Successfully sent message:", response);
-//         })
-//         .catch(function (error) {
-//             console.log("Error sending message:", error);
-//         });
-// }
 
 /**
  * This message send the notification to specified users whose registration token is placed here.
